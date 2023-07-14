c3e5dd42aad3844e7be4e065076cfc723190a5dd
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-13 20:23:44 -0600

    add Navigation component

---
+++ src/components/Navigation.js
@@ -0,0 +1,13 @@
+function Navigation() {
+    return (
+        <nav className="Navigation">
+            <span>Bookstore CMS</span>
+            <ul>
+                <li>BOOKS</li>
+                <li>CATEGORIES</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navigation;
