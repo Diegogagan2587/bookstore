c9bfb5fcb2249d5870bb3f31db28b5384ec48a90
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-13 19:59:38 -0600

    moves the router inside <app /> componenet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 
 import './App.css';
+import { createBrowserRouter, RouterProvider,} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h2>this is the root /</h2>,
+  }
+])
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
 //import libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider,} from "react-router-dom";
 // import Componentes and files
-import './index.css';
 import App from './App';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  }
-])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
 
