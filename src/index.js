664d1622495ef8ce7c5735ad5c288fa0f74afba2
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-13 19:52:06 -0600

    create a browser router

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
+//import libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider,} from "react-router-dom";
+// import Componentes and files
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
