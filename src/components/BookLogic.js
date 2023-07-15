b8bac3eea2adba142404e8c4c3c2401303209f38
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-14 20:50:26 -0600

    add a State for Books

--- src/components/BookLogic.js
+++ src/components/BookLogic.js
@@ -4,3 +4,31 @@ import BookList from "./BookList";
 import InputBook from "./InputBook";
 //Placeholder for importing
 
+const BookLogic = () => {
+  const [books, setBooks] = useState([
+    {
+      id: uuidv4(),
+      category: "action",
+      title: "The Hunger Games",
+      author: "Suzanne Collins",
+      completed: 64,
+      currentChapter: "Chapter 17",
+    },
+    {
+      id: uuidv4(),
+      category: "action",
+      title: "The Hunger Games",
+      author: "Suzanne Collins",
+      completed: 64,
+      currentChapter: "Chapter 3: A Lesson Learned",
+    },
+    {
+      id: uuidv4(),
+      category: "action",
+      title: "The Hunger Games",
+      author: "Suzanne Collins",
+      completed: 64,
+      currentChapter: "Introduction",
+    },
+  ]);
+
