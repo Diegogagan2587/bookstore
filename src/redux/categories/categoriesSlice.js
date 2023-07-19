aa2671f6476cb73d74aef19e8b228ed846a8cdf0
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-19 13:46:44 -0600

    define slice state for categories as categoriesSlice

---
+++ src/redux/categories/categoriesSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+const categoriesSlice = createSlice({
+  name: "categories",
+
+  reducers: {},
+});
+
+export default categoriesSlice.reducer;
