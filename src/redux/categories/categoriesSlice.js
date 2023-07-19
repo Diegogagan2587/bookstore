4410bbeeb91222dc41acbb1129bab3fa6a7757ff
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-19 13:47:59 -0600

    set inital state for array of categories as an empty array

--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -1,10 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
+const initialState = [];
 
 const categoriesSlice = createSlice({
   name: "categories",
-
+  initialState,
   reducers: {},
 });
 
