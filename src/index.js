2bc1865b674d9f568b02808fcea5c138711b7683
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-19 19:50:28 -0600

    Pass configured store into the Provider component

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import store from "./redux/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider  >
+    <Provider store={store} >
       <App />
     </Provider>
   </React.StrictMode>
