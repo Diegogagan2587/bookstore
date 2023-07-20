b3a53d061ff5cbc2707dace289eea30072d992b4
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-19 22:50:09 -0600

    refactor BookItem.js: Now will remove Books from store using dispatch

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,32 +1,40 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/booksSlice';
 
-const BookItem = ({ itemProp, delBook }) => (
-  <li className="book-container">
-    <div className="book-info">
-      <span className="genre">{itemProp.category}</span>
-      <h3 className="title">{itemProp.title}</h3>
-      <span className="author">{itemProp.author}</span>
-      <div className="edit-info">
-        <button type="button">Comments</button>
-        <hr />
-        <button type="button" onClick={() => delBook(itemProp.item_id)}>
-          Remove
-        </button>
-        <hr />
-        <button type="button">Edit</button>
+const BookItem = ({ itemProp }) => {
+  const dispatch = useDispatch();
+  return (
+    <li className="book-container">
+      <div className="book-info">
+        <span className="genre">{itemProp.category}</span>
+        <h3 className="title">{itemProp.title}</h3>
+        <span className="author">{itemProp.author}</span>
+        <div className="edit-info">
+          <button type="button">Comments</button>
+          <hr />
+          <button
+            type="button"
+            onClick={() => dispatch(removeBook(itemProp, itemProp.item_id))}
+          >
+            Remove
+          </button>
+          <hr />
+          <button type="button">Edit</button>
+        </div>
       </div>
-    </div>
-    <div className="progress">
-      <span className="progress-indicator">64%</span>
-      <span className="status-indicator">Completed</span>
-    </div>
-    <div className="chapter">
-      <span>CURRENT CHAPTER</span>
-      <span>Chapter 17</span>
-      <button type="button">UPDATE PROGRESS</button>
-    </div>
-  </li>
-);
+      <div className="progress">
+        <span className="progress-indicator">64%</span>
+        <span className="status-indicator">Completed</span>
+      </div>
+      <div className="chapter">
+        <span>CURRENT CHAPTER</span>
+        <span>Chapter 17</span>
+        <button type="button">UPDATE PROGRESS</button>
+      </div>
+    </li>
+  );
+};
 
 BookItem.propTypes = {
   itemProp: PropTypes.shape({
@@ -35,7 +43,6 @@ BookItem.propTypes = {
     author: PropTypes.string.isRequired,
     category: PropTypes.string.isRequired,
   }).isRequired,
-  delBook: PropTypes.func.isRequired,
 };
 
 export default BookItem;
