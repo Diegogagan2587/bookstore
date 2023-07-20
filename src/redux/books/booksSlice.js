ea156dab3bea6a1511d0963bfcd1ef8414092ff2
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-19 19:54:58 -0600

    set new initalState Value for booksSlice: include an array wiht 3 objects

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,13 +1,33 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = [];
+const initialState = [
+  {
+    item_id: "item1",
+    title: "The Great Gatsby",
+    author: "John Smith",
+    category: "Fiction",
+  },
+  {
+    item_id: "item2",
+    title: "Anna Karenina",
+    author: "Leo Tolstoy",
+    category: "Fiction",
+  },
+  {
+    item_id: "item3",
+    title: "The Selfish Gene",
+    author: "Richard Dawkins",
+    category: "Nonfiction",
+  },
+];
 
 const booksSlice = createSlice({
-  name: 'books',
+  name: "books",
   initialState,
   reducers: {
     addBook: (state, action) => state.push(action.payload),
-    removeBook: (state, action) => state.filter((book) => book.id !== action.payload),
+    removeBook: (state, action) =>
+      state.filter((book) => book.id !== action.payload),
   },
 });
 
