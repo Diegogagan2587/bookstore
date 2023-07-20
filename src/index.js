2547a34b83d96e1382e1a6361e04d6e3bbb93604
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-19 19:49:50 -0600

    wrap App component with Provider component imported from react-redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 // import libraries
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Provider from "react-redux";
 // import Componentes and files
-import App from './App';
+import App from "./App";
+import store from "./redux/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider  >
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
