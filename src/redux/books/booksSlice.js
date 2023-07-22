d7669f18174a5249d257ed8dd350b17a4fff0a80
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-22 12:46:12 -0600

    Add wrapper function for the logic that will get the data from the API

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,10 +1,16 @@
-import { createSlice,createAsyncThunk } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const fetchBooks = async (thunkAPI) => {
-  
-}
+  const app_id = 'lFZeV4dpbBKDSSANp21d';
+  const url_api = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${app_id}/`;
+  try {
+    //Here I need to fetch data  and assing a response;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
+};
 
-const getBooksFromAPI = createAsyncThunk('books/fetch',fetchBooks);
+const getBooksFromAPI = createAsyncThunk('books/fetch', fetchBooks);
 
 const initialState = [
   {
@@ -34,11 +40,11 @@ const booksSlice = createSlice({
     addBook: (state = initialState, action) => {
       state.push(action.payload);
     },
-    removeBook: (state = initialState, action) => state.filter(
-      (book) => book.item_id !== action.payload.item_id,
-    ),
+    removeBook: (state = initialState, action) =>
+      state.filter((book) => book.item_id !== action.payload.item_id),
   },
 });
 
+export { getBooksFromAPI };
 export const { addBook, removeBook } = booksSlice.actions;
 export default booksSlice.reducer;
