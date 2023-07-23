dad0595ff3c3190e98141170f904896ce246a085
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-22 18:20:50 -0600

    remove initialState placeholder objects

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -33,24 +33,6 @@ const getBooksFromAPI = createAsyncThunk('books/fetch', fetchBooks);
 const postBooksToAPI = createAsyncThunk('books/post', submitBooks);
 
 const initialState = [
-  {
-    item_id: 'item1',
-    title: 'The Great Gatsby',
-    author: 'John Smith',
-    category: 'Fiction',
-  },
-  {
-    item_id: 'item2',
-    title: 'Anna Karenina',
-    author: 'Leo Tolstoy',
-    category: 'Fiction',
-  },
-  {
-    item_id: 'item3',
-    title: 'The Selfish Gene',
-    author: 'Richard Dawkins',
-    category: 'Nonfiction',
-  },
 ];
 
 const booksSlice = createSlice({
