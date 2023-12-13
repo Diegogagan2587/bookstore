b311a58727dae1a56fc2925654da19e315adb26a
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-12-13 16:08:33 -0600

    Update categoreis link

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,7 +6,7 @@ function Navigation() {
       <span className="book-cms">Bookstore CMS</span>
       <ul>
         <li><Link to="/"><span className="nav-text">BOOKS</span></Link></li>
-        <li><Link to="/categories"><span className="nav-text">CATEGORIES</span></Link></li>
+        <li><Link to="categories"><span className="nav-text">CATEGORIES</span></Link></li>
       </ul>
     </nav>
   );
